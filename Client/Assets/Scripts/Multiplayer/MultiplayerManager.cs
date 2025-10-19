c84b2af16640e7a5821fad0dcb73d6418cb34f20
FalaleevDanila <falaleevdanila@gmail.com>
2025-10-19 12:36:08 +0300

    add 2 player (but game is not ready)

--- Client/Assets/Scripts/Multiplayer/MultiplayerManager.cs
+++ Client/Assets/Scripts/Multiplayer/MultiplayerManager.cs
@@ -1,8 +1,11 @@
 using Colyseus;
+using Unity.VisualScripting;
+using UnityEngine;
 
 public class MultiplayerManager : ColyseusManager<MultiplayerManager> {
     private ColyseusRoom<State> _room;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] private GameObject _player;
+    [SerializeField] private GameObject _enemy;
     protected override void Awake()
     {
         base.Awake();
@@ -12,8 +15,29 @@ public class MultiplayerManager : ColyseusManager<MultiplayerManager> {
     private async void Connect()
     {
         _room = await Instance.client.JoinOrCreate<State>("state_handler");
+        _room.OnStateChange += OnChange;
     }
 
+    private void OnChange(State state, bool isFirstState)
+    {
+        if (!isFirstState)
+            return;
+        var player = state.players[_room.SessionId];
+        var position = new Vector3(player.x - 200, 0, player.y - 200) / 8;
+
+        Instantiate(_player, position, Quaternion.identity);
+
+        state.players.ForEach(ForEachEnemy);
+    }
+
+    private void ForEachEnemy(string key, Player player)
+    {
+        if (key == _room.SessionId)
+            return;
+        var position = new Vector3(player.x - 200, 0, player.y - 200) / 8;
+
+        Instantiate(_enemy, position, Quaternion.identity);
+    }
     protected override void OnDestroy()
     {
         base.OnDestroy();
